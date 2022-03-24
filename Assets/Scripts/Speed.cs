c0b398222c3f94e785893add218ac477e77e9ae3
unknown <glados@17R4-E.insper.local>
2022-03-24 17:39:42 -0300

    Campo de forca criado~

--- Assets/Scripts/Speed.cs
+++ Assets/Scripts/Speed.cs
@@ -9,6 +9,7 @@ public class Speed : MonoBehaviour
 {
     // public SteamVR_Action_Boolean botao = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
     // FixedJoint joint = null;
+    public Rigidbody rb;
     public float MoveSpeed = 3.0f;
     // SteamVR_Behaviour_Pose trackedObj;
 
@@ -16,6 +17,10 @@ public class Speed : MonoBehaviour
     void Start()
     {
         // trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
+        rb = GetComponent<Rigidbody>();
+
+// PEGAR o RIGID BODY rg getcomponenet rigidbody
+
     }
 
     // Update is called once per frame
@@ -24,10 +29,22 @@ public class Speed : MonoBehaviour
         // if(botao.GetStateUp(trackedObj.inputSource)){
         //     transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
         // }
+
+
+        // PEGAR O PARENT E CHECAR O NOME 
+        Vector3 vel = rb.velocity;
+        if(rb.transform.parent == null){
+            rb.velocity = transform.forward * MoveSpeed;
+        }
+
+        // rb.velocity (transform.forward * 100);
+
+        print(transform.forward);
     }
 
-    void OnDetachedFromHand(Hand hand)
+    void HandAttachedUpdate(Hand hand)
     {
-        transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        //print("largou"); 
+        //transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
     }
 }
