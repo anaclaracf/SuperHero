a588c3c0c8d0fbaaf755e09c89f4dc47e35979e0
unknown <glados@17R4-E.insper.local>
2022-03-24 17:51:46 -0300

    atualização

--- Assets/Scripts/ShieldBP.cs
+++ Assets/Scripts/ShieldBP.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.Extras;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class ShieldBP : MonoBehaviour
 {
     // Start is called before the first frame update
-    // public SteamVR_Action_Boolean botao = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+    public SteamVR_Action_Boolean botao = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
+    SteamVR_Behaviour_Pose trackedObj;
+
     void Start()
     {
         
@@ -14,6 +19,11 @@ public class ShieldBP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(botao.GetStateDown(trackedObj.inputSource)){
+            transform.this.SetActive(true);
+        }
+        else{
+            transform.this.SetActive(false);
+        }
     }
 }
