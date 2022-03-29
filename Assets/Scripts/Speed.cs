a41c968f2e0000798fe94caf6b2e3e5899c502a0
unknown <glados@17R4-E.insper.local>
2022-03-29 18:54:59 -0300

    atualizacoes

--- Assets/Scripts/Speed.cs
+++ Assets/Scripts/Speed.cs
@@ -11,31 +11,44 @@ public class Speed : MonoBehaviour
     // FixedJoint joint = null;
     public Rigidbody rb;
     public float MoveSpeed = 3.0f;
-    // SteamVR_Behaviour_Pose trackedObj;
 
+    Vector3 direcao;
+
+    // SteamVR_Behaviour_Pose trackedObj;
+GameObject hand ;
     // Start is called before the first frame update
     void Start()
     {
         // trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
         rb = GetComponent<Rigidbody>();
 
+        hand = GameObject.Find("RightHand");
+
 // PEGAR o RIGID BODY rg getcomponenet rigidbody
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(GetComponent<FixedJoint>() != null){
+            direcao = hand.transform.;
+        }
+
+        //print(transform.parent);
+
         // if(botao.GetStateUp(trackedObj.inputSource)){
         //     transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
         // }
 
 
         // PEGAR O PARENT E CHECAR O NOME 
-        Vector3 vel = rb.velocity;
-        if(rb.transform.parent == null){
-            rb.velocity = transform.forward * MoveSpeed;
-        }
+
+        
+
+        //Vector3 vel = rb.velocity;
+        //if(rb.transform.parent == null){
+            rb.velocity = direcao * MoveSpeed;
+        //}
 
         // rb.velocity (transform.forward * 100);
 
